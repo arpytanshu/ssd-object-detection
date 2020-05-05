Fixes in dataset - fixed horizontal flipping of images and boxes only in case of train with a prob of 0.5 - Normalization of images with torchvision mean and SD is now commented in imageTransforms. - evaluation code is now in tainer.py::eval() => cleaned evaluation.py

data.py
@@ -8,7 +8,9 @@ Created on Tue Apr 17 01:17:07 2020
 
 from PIL import Image
 from os import listdir
+from random import uniform
 from torch.utils.data import Dataset
+
 
 import utils
 import torch
@@ -48,11 +50,9 @@ def hflip(image, boxes):
     image : a PIL Image
     boxes : a tensor of dimensions (n_objects, 4)
         Bounding box in [ x_min, y_min, x_max, y_max ] format.
-
     Returns
     -------
     flipped image, updated bounding box coordinates.
-
     '''
     # Flip image
     new_image = FT.hflip(image)
@@ -81,9 +81,9 @@ def imageTransforms(image):
 
     '''
     ImageTransforms = T.Compose([
-            T.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.25),
+            T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
             T.ToTensor(),
-            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            # T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
     new_image = ImageTransforms(image)
     return new_image
@@ -122,8 +122,10 @@ def collate_fn(batch):
    
    
 class ShelfImageDataset(Dataset):
-    def __init__(self, df, image_path, train=True):
+    def __init__(self, df, image_path, train=True, return_orig=False):
         self.df = df
+        self.train = train
+        self.return_orig = return_orig
         self.image_path = image_path+'train/' if train else image_path+'test/'
         self._fix_df()
         
@@ -136,11 +138,17 @@ class ShelfImageDataset(Dataset):
         self.df.reset_index(drop=True, inplace=True)
         
     def __getitem__(self, idx):
-        image = Image.open(self.image_path + self.df.loc[idx, 'image_name']).convert('RGB')
+        orig_image = Image.open(self.image_path + self.df.loc[idx, 'image_name']).convert('RGB')
         boxes = torch.tensor(self.df.loc[idx, 'BB_xywh'])
         boxes = utils.xywh_to_xyXY(boxes)
-        image, boxes = hflip(image, boxes)
-        image, boxes = resize(image, boxes, (300,300))
+        # rescale image and boxes
+        image, boxes = resize(orig_image, boxes, (300,300))
+        # horizontal flip image and boxes with 50% prob (if it's a train sample)
+        if (uniform(0,1)>0.5 and self.train): 
+            image, boxes = hflip(image, boxes)
         image = imageTransforms(image)
         label = torch.LongTensor([1]*boxes.size(0))
-        return image, boxes, label
+        if self.return_orig:
+            return image, boxes, label, orig_image
+        else:
+            return image, boxes, label

evaluation.py
@@ -11,13 +11,9 @@ import torch
 from ssdconfig import SSDConfig
 from data import ShelfImageDataset, collate_fn, get_dataframe
 from torch.utils.data import DataLoader
-from tqdm import tqdm
-from ssdutils import calc_mAP
-
-#%%
+from trainer import eval
 
 config = SSDConfig()
-
 
 df = get_dataframe(config.PATH_TO_ANNOTATIONS)
 dataset = ShelfImageDataset(df, config.PATH_TO_IMAGES, train=False)
@@ -28,45 +24,12 @@ dataloader = DataLoader(dataset,
                         num_workers=config.NUM_DATALOADER_WORKERS)
 
 
-
 ckpt_path = './checkpoints/checkpoint_ssd_1.pth.tar'
 # ckpt_path = 'checkpoints/checkpoint_ssd_2-1AP.pth.tar'
 checkpoint = torch.load(ckpt_path, map_location=torch.device('cpu'))
 model = checkpoint['model']
 model.config.DEVICE = torch.device('cpu')
-model.eval()
 
+mAP = eval(model, dataloader, min_score=0.5, max_overlap=0.5)
 
-#%%
-
-min_score = 0.4
-max_overlap = 0.4
-
-gt_boxes = []
-gt_labels = []
-pred_boxes = []
-pred_labels = []
-pred_scores = []
-
-for batch in tqdm(dataloader):
-    
-    orig_images = batch[0]
-    orig_boxes = batch[1]
-    orig_labels = batch[2]
-    
-    
-    with torch.no_grad():
-        loc_gcxgcy, scores = model(orig_images)
-        boxes, labels, scores = model.detect_objects(loc_gcxgcy, scores, min_score=min_score, max_overlap=max_overlap)
-
-    gt_boxes.extend(orig_boxes)
-    gt_labels.extend(orig_labels)
-    pred_boxes.extend(boxes)
-    pred_labels.extend(labels)
-    pred_scores.extend(scores)
-
-
-AP, mAP = calc_mAP(gt_boxes, gt_labels, pred_boxes, pred_labels, pred_scores)
 print(mAP)
-
-